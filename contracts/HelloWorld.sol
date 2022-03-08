ffda543793914cec3b91d9d92a94f652d3192ade
joseph achonu <josephachonu@gmail.com>
2022-03-08 19:16:00 +0100

    made changes

--- contracts/HelloWorld.sol
+++ contracts/HelloWorld.sol
@@ -1,8 +1,10 @@
 //SPDX-License-Identifier: Unlicensed
+pragma solidity ^0.8.1;
 
-pragma solidity ^0.8.0;
+import "./Ownable.sol";
+import "hardhat/console.sol";
 
-contract HelloWorld {
+contract HelloWorld is Ownable {
     mapping(address => Message) blockgameMessages;
     Message[] blockgameMessagesArray;
     address blockGamesAddress;
@@ -16,18 +18,20 @@ contract HelloWorld {
     constructor() {}
 
     function sendBlockGameMessage(string memory message) external {
+        console.log("%s is sending blockgames a message.", msg.sender);
         bytes memory messageByte = bytes(message);
         require(messageByte.length > 0, "Message cannot be empty.");
         blockgameMessagesArray.push(Message(message, block.timestamp));
+        blockgameMessages[msg.sender] = Message(message, block.timestamp);
         emit NewMessage(msg.sender, message, blockgameMessageCount);
     }
 
     function getBlockgameMessage() public view returns (Message memory) {
-        //Message memory _msg= new Message(blockgameMessages[msg.sender].Message,blockgameMessages[msg.sender].CreateAt);
+        console.log("getter address: %s", msg.sender);
         return blockgameMessages[msg.sender];
     }
 
-    function setBlockGameAddress(address _address) external {
+    function setBlockGameAddress(address _address) external onlyOwner {
         blockGamesAddress = _address;
     }
 
